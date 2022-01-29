bb7341011ccac9c1f4f8e382752e9bf953f0d8dd
Simon Schrape <sim@ilitu.de>
2022-01-29 11:30:27 +0100

    Handle deprecations. * Remove deprecated Linkify option. * Replace deprecated Highlight syntax.   (https://github.com/highlightjs/highlight.js/issues/2277)

--- static/js/md.js
+++ static/js/md.js
@@ -6,7 +6,6 @@ const md = new Remarkable("full", {
   xhtmlOut: true, // Use '/' to close single tags (<br />)
   breaks: false, // Convert '\n' in paragraphs into <br>
   langPrefix: "language-", // CSS language prefix for fenced blocks
-  linkify: false, // autoconvert URL-like texts to links
   linkTarget: "", // set target to open link in
 
   // Enable some language-neutral replacements + quotes beautification
@@ -21,7 +20,7 @@ const md = new Remarkable("full", {
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return hljs.highlight(lang, str).value;
+        return hljs.highlight(str, { language: lang }).value;
       } catch (__) {}
     }
 
